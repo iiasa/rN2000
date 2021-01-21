3f1fe5ddec2b73ed42ca15e88ff5fac729363a0a
Martin Jung <3788377+Martin-Jung@users.noreply.github.com>
2021-01-21 15:39:47 +0100

    Matts additions (#8)
    * bug fix of folder
    
    * bug fixes to n2000_get()
    
    * bug fixes to n2000_get
    
    * bug fixes to n2000_get for windows unzipping
    
    * n2000_extract()
    
    * manual updates
    
    * bug fixes to n2000_extract()
    
    Co-authored-by: mathewlewis896 <matthewlewis896@gmail.com>

--- R/n2000_get.R
+++ R/n2000_get.R
@@ -13,7 +13,7 @@
 n2000_getCSV <- function(folder,
                       year = "2019",
                       n_link = 'https://www.eea.europa.eu/data-and-maps/data/natura-11/natura-2000-tabular-data-12-tables/natura-2000-comma-separated-values-files/at_download/file'){
-  folder <-
+  folder2 <-
     ifelse(
       substr(folder, nchar(folder), nchar(folder)) == "/",
       substr(folder, 1, (nchar(folder) -1)),
@@ -21,7 +21,7 @@ n2000_getCSV <- function(folder,
     )
 
   # Check
-  assert_that(is.dir(folder),
+  assert_that(is.dir(folder2),
               is.character(year))
 
   # Key files and sources
@@ -32,18 +32,19 @@ n2000_getCSV <- function(folder,
 
   # if zip file already there, unzip and delete
   if(file_zipped %in% basename(lf)){
-    unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ],exdir = folder,overwrite = TRUE)
+    unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ],exdir = paste0(folder, gsub(".zip", "", file_zipped)),overwrite = TRUE)
     file.remove(lf[ grep(file_zipped, basename(lf)) ])
   } else {
     # Check if some of the zipped files do not exist
     if( paste0('Natura2000_end',year,'_SPECIES.csv') %notin% basename(lf) ) {
       myLog('No Natura 2000 data found. Redownloading')
-      try({ o <- download.file(url = n_link,
-                    destfile = paste0(folder,'/', file_zipped))
+      o<- try({ download.file(url = n_link,
+                    destfile = paste0(folder,'/', file_zipped), mode = "wb")
       })
       if(class(o)=='try-error' || o != 0){ stop('Natura 2000 file could not be downloaded. Change download link')}
       # Unzip and delete the downloaded file
-      unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ],exdir = folder,overwrite = TRUE)
+      lf <- list.files(folder,full.names = T)
+      unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ],exdir = paste0(folder, gsub(".zip", "", file_zipped)),overwrite = TRUE)
       file.remove(lf[ grep(file_zipped, basename(lf)) ])
     }
   }
@@ -64,7 +65,7 @@ n2000_getCSV <- function(folder,
 n2000_getGPGK <- function(folder,
                          year = "2019",
                          n_link = 'https://cmshare.eea.europa.eu/s/GkqdcbbsYmmBSEQ/download'){
-  folder <-
+  folder2 <-
     ifelse(
       substr(folder, nchar(folder), nchar(folder)) == "/",
       substr(folder, 1, (nchar(folder) -1)),
@@ -72,7 +73,7 @@ n2000_getGPGK <- function(folder,
     )
 
   # Check
-  assert_that(is.dir(folder),
+  assert_that(is.dir(folder2),
               is.character(year))
 
   # Key files and sources
@@ -82,20 +83,21 @@ n2000_getGPGK <- function(folder,
   lf <- list.files(folder,full.names = T)
 
   # if zip file already there, unzip and delete
-  if(file_zipped %in% basename(lf)){
-    unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ], exdir = folder, overwrite = TRUE)
-    file.remove(lf[ grep(file_zipped, basename(lf)) ])
+  if(file_g %in% basename(lf)){
+    unzip(zipfile = lf[ grep(file_g, basename(lf)) ], exdir = paste0(folder, gsub(".zip", "", file_g)), overwrite = TRUE)
+    file.remove(lf[ grep(file_g, basename(lf)) ])
   } else {
     # Check if some of the zipped files do not exist
     if( paste0('Natura2000_end',year,'.gpkg') %notin% basename(lf) ) {
       myLog('No Natura 2000 geopackage found. Redownloading...')
-      try({ o <- download.file(url = n_link,
-                               destfile = paste0(folder,'/', file_zipped))
+      o<- try({ download.file(url = n_link,
+                              destfile = paste0(folder,'/', file_g), mode = "wb")
       })
       if(class(o)=='try-error' || o != 0){ stop('Natura 2000 file could not be downloaded. Change download link')}
       # Unzip and delete the downloaded file
-      unzip(zipfile = lf[ grep(file_zipped, basename(lf)) ], exdir = folder, overwrite = TRUE)
-      file.remove(lf[ grep(file_zipped, basename(lf)) ])
+      lf <- list.files(folder,full.names = T)
+      unzip(zipfile = lf[ grep(file_g, basename(lf)) ], exdir = paste0(folder, gsub(".zip", "", file_g)), overwrite = TRUE)
+      file.remove(lf[ grep(file_g, basename(lf)) ])
     }
   }
 }
